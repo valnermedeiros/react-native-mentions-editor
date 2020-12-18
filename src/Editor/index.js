afe61bb98a29fc63323e035e74dfe2cd7f243d0e
Valner Medeiros <valner.coding@gmail.com>
2020-12-18 00:10:00 -0300

    Comment selection on text input to fix rn 0.63 ios cursor issue

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -569,7 +569,7 @@ export class Editor extends React.Component {
                 value={state.inputText}
                 onBlur={props.toggleEditor}
                 onChangeText={this.onChange}
-                selection={this.state.selection}
+                //selection={this.state.selection}
                 selectionColor={"#000"}
                 onSelectionChange={this.handleSelectionChange}
                 placeholder={state.placeholder}
